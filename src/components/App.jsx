9061a2437dba0008ff343616ecbde64b027caad3
YriyProtcyshin <105303233+YriyProtcyshin@users.noreply.github.com>
2022-12-30 22:40:58 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 import { Home } from "page/Home/Home";
 import { lazy, Suspense } from "react";
-// import Movie from "page/Movie/Movies";
-// import MoviesDetail from "page/MoviesDetail/MoviesDetail";
 import Cast from "./Cast/Cast";
 import Reviews from "./Reviews/Reviews";
 
 const Movie = lazy(() => import("../page/Movie/Movies"))
 const MoviesDetail = lazy(() => import("../page/MoviesDetail/MoviesDetail"))
-// const Cast = lazy(() => import("../components/Cast/Cast"))
-// const Reviews = lazy(() => import("../components/Reviews/Reviews"))
-
 
 export const App = () => {
   return (
